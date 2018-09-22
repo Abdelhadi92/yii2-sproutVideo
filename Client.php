497a6a20b912fbf1180b2849abb2391dd66938cf
Abdelhadi Abu Shamleh <abushamleh@Abdelhadis-MacBook-Pro.local>
2018-09-23 00:02:02 +0300

    Add main classes

---
+++ Client.php
@@ -0,0 +1,36 @@
+<?php
+namespace abushamleh\sproutVideo;
+
+use yii\base\Component;
+use SproutVideo;
+
+
+/**
+ *
+ * @property SproutVideo\AccessGrant $accessGrant
+ * @property SproutVideo\Account $account
+ * @property SproutVideo\Analytics $analytics
+ * @property SproutVideo\Login $login
+ * @property SproutVideo\Playlist $playlist
+ * @property SproutVideo\Tag $tag
+ * @property SproutVideo\UploadToken $uploadToken
+ * @property SproutVideo\Video $video
+ */
+class Client extends Component
+{
+    public $api_key;
+
+    public function init()
+    {
+        SproutVideo::$api_key = $this->api_key;
+    }
+
+    public function __get($name)
+    {
+        $class = 'SproutVideo\\'.$name;
+        if (class_exists($class)){
+            return $class;
+        }
+        return parent::__get($name);
+    }
+}
